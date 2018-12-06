f9fcd5de4bc991968f63f73281507b341fb12813
malk <peuple_boy@163.com>
2018-12-06 23:44:19 +0800

    Update main.js

--- vue-universal-h5/src/main.js
+++ vue-universal-h5/src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 /** native 和 api 按需引入，内部使用export方式；calc的export default为对象，也建议是使用到再引入（还能有代码提示） */
 
 // Rem适配方案
-import Rem from './utils/rem'
+import Rem from './config/rem'
 Rem.rem();
 
 // 全局引入 mint-ui
