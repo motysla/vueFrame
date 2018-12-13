7a88ddfeb2283c0fc2a987445c1c5cfa0f6570f5
malk <peuple_boy@163.com>
2018-12-13 17:41:56 +0800

    Update login.vue

---
+++ vue-universal-h5/src/views/auth/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main">
+    login
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed: { },
+  watch: { },
+  mounted() {
+
+  },
+}
+
+</script>
+
+<style scoped lang="less">
+
+.main {
+
+}
+</style>
+
