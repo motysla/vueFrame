e383e50a86ecd80c4aa00d0c645eac7379de9eba
malk <peuple_boy@163.com>
2018-12-13 17:41:51 +0800

    Update router.js

--- vue-universal-h5/src/router.js
+++ vue-universal-h5/src/router.js
@@ -4,23 +4,34 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-	routes: [{
-			// 启动路径
-			path: '/',
-			redirect: "/index"
-		},
-		{
-			// 404 页面重定向
-			path: '*',
-			redirect: "/index"
-		},
-		{
-			path: '/index',
-			name: "index",
-			meta: {
-				title: "首页"
-			},
-			component: r => require.ensure([], () => r(require('./views/index.vue')), 'index')
-		},
-	]
+  routes: [
+    {
+      // 启动路径
+      path: '/',
+      redirect: '/index'
+    },
+    {
+      // user-404 页面重定向
+      path: 'user-*',
+      redirect: '/index'
+    },
+    {
+      path: '/index',
+      name: 'index',
+      meta: {
+        title: '首页'
+      },
+      component: r =>
+        require.ensure([], () => r(require('./views/index.vue')), 'index')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      meta: {
+        title: '登录 - 注册'
+      },
+      component: r =>
+        require.ensure([], () => r(require('./views/auth/login.vue')), 'login')
+    },
+  ]
 })
